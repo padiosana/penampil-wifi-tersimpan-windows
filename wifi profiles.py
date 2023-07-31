2e32e13853801af67a0c332d1d184fcdbbe3249b
padiosana <84434840+padiosana@users.noreply.github.com>
2023-07-31 10:47:53 +0700

    Update wifi profiles.py

--- wifi profiles.py
+++ wifi profiles.py
@@ -1,42 +1,81 @@
+import os
 import subprocess
+import multiprocessing
 
 def save_log(log):
-    file_path = input("Enter the file path to save the log: ")
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    file_name = "wifi_passwords_log.txt"
+    file_path = os.path.join(script_dir, file_name)
+    
     with open(file_path, "w") as f:
         f.write(log)
-    print("Log saved successfully!")
+    print(f"\nLog saved successfully to: {file_path}\n")
 
-profiles = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output=True).stdout
+def scan_wifi():
+    profiles = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output=True).stdout
 
-lines = profiles.splitlines()
+    lines = profiles.splitlines()
 
-log = ""
+    ssid_list = [line.split(b":")[1].strip() for line in lines if b"Profile" in line]
 
-for line in lines:
-    if b"Profile" in line:
-        profile_name = line.split(b":")[1].strip()
+    with multiprocessing.Pool() as pool:
+        results = pool.map(scan_password, ssid_list)
 
-        result = subprocess.run(["netsh", "wlan", "show", "profile", profile_name, "key=clear"], capture_output=True)
+    ssid_passwords = [(ssid, password) for ssid, password in results if password]
 
-        lines = result.stdout.splitlines()
+    # Mengurutkan SSID berdasarkan huruf dan kapitalisasi
+    ssid_passwords = sorted(ssid_passwords, key=lambda x: x[0].lower())
 
-        for line in lines:
-            if b"Key Content" in line:
-                password = line.split(b":")[1].strip()
-                log += f"---------------------------\n"
-                log += f"SSID     : {profile_name.decode()}\n"
-                log += f"password : {password.decode()}\n"
-                log += f"---------------------------\n"
-                print(f"---------------------------")
-                print(f"SSID     : {profile_name.decode()}")
-                print(f"password : {password.decode()}")
-                print(f"---------------------------")
+    return ssid_passwords
 
-while True:
-    choice = input("Enter 's' to save the log, 'e' to exit: ")
-    if choice == "s":
-        save_log(log)
-    elif choice == "e":
-        break
-    else:
-        print("Invalid choice. Please try again.")
+def scan_password(ssid):
+    result = subprocess.run(["netsh", "wlan", "show", "profile", ssid, "key=clear"], capture_output=True)
+    lines = result.stdout.splitlines()
+
+    for line in lines:
+        if b"Key Content" in line:
+            password = line.split(b":")[1].strip()
+            return ssid.decode(), password.decode()
+
+    return ssid.decode(), None
+
+def print_wifi(ssid_passwords):
+    log = "No.  SSID                           Password\n"
+    log += "--------------------------------------------\n"
+
+    print("No.  SSID                           Password")
+    print("--------------------------------------------")
+
+    for idx, (ssid, password) in enumerate(ssid_passwords, start=1):
+        ssid_format = f"{idx:2}.  {ssid:<30}  {password}"
+        log += f"{ssid_format}\n"
+
+        print(ssid_format)
+
+    log += "--------------------------------------------\n"
+    return log
+
+def main():
+    ssid_passwords = scan_wifi()
+    log = print_wifi(ssid_passwords)
+
+    while True:
+        print("\nMenu:")
+        print("1. Save log")
+        print("2. Rescan Wi-Fi")
+        print("3. Exit")
+        choice = input("Enter the number of your choice: ")
+
+        if choice == "1":
+            save_log(log)
+        elif choice == "2":
+            print("\nScanning Wi-Fi networks...\n")
+            ssid_passwords = scan_wifi()
+            log = print_wifi(ssid_passwords)
+        elif choice == "3":
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
